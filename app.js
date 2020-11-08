88d73a7dfcaecc5277a8c137203cc4aae5979530
박철현 <parkcheolhyun@cheolmaegi.local>
2020-11-08 23:03:09 +0900

    # 로그인까지만 (JWT 처리 필요)

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var logger = require('morgan');
 
 var sequelize = require('./models').sequelize;
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
+var userRouter = require('./routes/user');
 
 var app = express();
 
@@ -24,8 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,10 @@ const omissionChecker = require('../lib/omissionChecker');
 
 const { User } = require('../models');
 
-router.post('/signup', async function (req, res, next) {
+router.post('/', async function (req, res, next) {
     /*
-        회원가입 요청.
-        path: /api/auth/signup.  
+        회원가입 요청.(POST)
+        path: /api/user.
     */
     if (req.body !== {}) {
         const { email, name, birth, phone_number, password } = req.body;
@@ -20,16 +20,16 @@ router.post('/signup', async function (req, res, next) {
             /*
                 모든 데이터가 정상적으로 넘어왔으면 회원가입 절차 실행.
             */
-            const findData = await User.findOne({ where: { email } });
+            const exUser = await User.findOne({ where: { email } });
             // 이미 가입된 이메일이 있는지 찾음.
-            if (!findData) {
+            if (!exUser) {
                 // 일치하는 이메일이 없다 => 가입 가능한 계정.
                 const hash = await bcrypt.hash(password, 12);
-                const createData = await User.create({
+                const createUser = await User.create({
                     email, name, password: hash, phone_number,
                     birth: new Date(birth),
                 });
-                if (createData) {
+                if (createUser) {
                     // 가입 성공이라는 메세지 보냄.
                     res.send({ msg: 'success' });
                 }
@@ -43,6 +43,34 @@ router.post('/signup', async function (req, res, next) {
         }
     } else {
         // 데이터를 보내지 않았을 때.
+        res.send({ msg: '정상적으로 데이터를 전송하지 않음.' });
+    }
+});
+
+router.post('/signin', async function (req, res, next) {
+    /*
+        로그인 요청.(POST)
+        path: /api/user/signin
+    */
+    if (req.body !== {}) {
+        const { email, password } = req.body;
+        const omissionResult = omissionChecker({ email, password });
+        if (omissionResult.result) {
+            const exUser = await User.findOne({ where: { email }});
+            if (exUser) {
+                const result = await bcrypt.compare(password, exUser.password);
+                if (result) {
+                    res.send({ msg: '로그인 성공!' });
+                } else {
+                    res.send({ msg: '비밀번호가 일치하지 않습니다.' });
+                }
+            } else {
+                res.send({ msg: '가입되지 않은 이메일입니다.' });
+            }
+        } else {
+            res.send({ msg: omissionResult.message });
+        }
+    } else {
         res.send({ msg: '정상적으로 데이터를 전송하지 않음.' });
     }
 });
