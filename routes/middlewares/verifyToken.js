6f7a4e4241ed23d770fdd752cdc8702a00be2a31
ghooz1204 <ghooz1204@naver.com>
2020-12-13 15:34:47 +0900

    # 좋아요 확인 api

--- routes/middlewares/verifyToken.js
+++ routes/middlewares/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     try {
         if (!req.headers.authorization) {
-            res.status(401).json({ msg: 'please request token' });
+            res.status(401).json({ msg: '로그인 후에 이용해 주세요.' });
         }
         const CLIENT_TOKEN = req.headers.authorization.split('Bearer ')[1];
         const decoded = jwt.verify(CLIENT_TOKEN, process.env.JWT_SECRET);
@@ -13,13 +13,13 @@ const verifyToken = (req, res, next) => {
                 req.decodeToken = decoded;
                 next();
             } else {
-                res.status(401).json({ msg: 'unauthorized' });
+                res.status(401).json({ msg: '잘못된 로그인 정보입니다.' });
             }
         } else {
-            res.status(401).json({ msg: 'unauthorized' });
+            res.status(401).json({ msg: '잘못된 로그인 정보입니다.' });
         }
     } catch (err) {
-        res.status(401).json({ msg: 'token expired', err });
+        res.status(401).json({ msg: '잘못된 로그인 정보입니다.' });
     }
 };
 
