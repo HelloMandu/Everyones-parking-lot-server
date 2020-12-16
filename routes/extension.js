68c6396f642749376c39af47be143e14d9b01c93
root <root@vultr.guest>
2020-12-16 08:55:29 +0000

    #modify extension, rental feeTime" ;

--- routes/extension.js
+++ routes/extension.js
@@ -102,13 +102,14 @@ router.post('/', verifyToken, async (req, res, next) => {
             // 대여 종료 시간이 대여 시작 시간보다 앞이면 오류.
             return res.status(202).send({ msg: '잘못 설정된 대여 시간입니다.' });
         }
-        const feeTime = Math.ceil(diffTime / ((30 * MINUTE) - 1)); // 30분으로 나눴을 때 나오는 수 * 요금이 전체 요금.
+	const minusTime = Math.ceil(diffTime % (30 * MINUTE)) === 0 ? 0 : 1;
+        const feeTime = Math.ceil(diffTime / (30 * MINUTE) - minusTime); // 30분으로 나눴을 때 나오는 수 * 요금이 전체 요금.
         if (feeTime <= 1) {
             // 대여 시간이 30분 이하일 경우
             return res.status(202).send({ msg: '최소 대여 시간보다 적게 대여할 수 없습니다.' });
         }
         if (extensionPrice !== place_fee * feeTime) {
-            return res.status(202).send({ msg: '잘못 계산된 금액입니다.' });
+           return res.status(202).send({ msg: '잘못 계산된 금액입니다.' });
         }
         // 운영시간과 겹치는지 안 겹치는지.
         if (!moment(rentalEndTime).isBetween(oper_start_time, oper_end_time, undefined, "[]")
