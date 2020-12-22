199aef1f2321bacae651deaf32b7916493885965
ghooz1204 <ghooz1204@naver.com>
2020-12-22 12:58:03 +0900

    # firebase 취소

--- actions/pushSender.js
+++ actions/pushSender.js
@@ -1,9 +1,5 @@
 const admin = require('firebase-admin');
 
-admin.initializeApp({
-    credential: admin.credential.cert(process.env.PUSH_KEY)
-});
-
 /*
     특정 API Method를 수행한 후 그 행위에 대한 알림을
     디바이스 푸쉬로 전달하기 위한 Actions
@@ -23,8 +19,8 @@ const sendPushNotification = async (native_token, title, body) => {
                     direct_boot_ok: true,
                 },
             };
-            const result = admin.messaging().send(message);
-            console.log(result);
+            // const result = admin.messaging().send(message);
+            // console.log(result);
 
         } catch (err) {
             console.error(err);
